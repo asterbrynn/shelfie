59e82b0a35e20c68ef4bc5cf9319a4312cc902a8
Aster Brynn <aster.gibby@gmail.com>
2019-04-19 10:34:14 -0600

    initial set up

---
+++ server/index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const massive = require('massive');
+const controller = require('./controller');
+
+const {SERVER_PORT, CONNECTION_STRING} = process.env;
+const app = express();
+app.use(express.json());
+
+massive(CONNECTION_STRING).then(dbInstance => {
+	app.set("db", dbInstance);
+	app.listen(SERVER_PORT, () => console.log(`listening on port ${SERVER_PORT}`))
+}).catch(err => console.log("oopsies!", err))
---
+++ src/Components/Form/Form.js
@@ -0,0 +1,17 @@
+import React, {Component} from 'react';
+
+export default class Form extends Component {
+	constructor() {
+		super();
+		this.state = {
+			name: "",
+			price: 0,
+			imgurl: ""
+		}
+	}
+	render() {
+		return (
+			<div>Form</div>
+		)
+	}
+}
